9e1529096dc51cfb0b001cae5f5c999c495126ef
Chitrasyahriel <chitrasyahriel@gmail.com>
2019-08-12 19:03:07 +0700

    second commit

---
+++ app/src/main/java/com/example/choleved/RESULT.java
@@ -0,0 +1,13 @@
+package com.example.choleved;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RESULT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
